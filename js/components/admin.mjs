5921ddcfe34f97b076c84edd3f3ca475fe4a1110
andjoa00220@stud.noroff.no <andjoa00220@stud.noroff.no>
2024-05-27 12:56:28 +0200

    pathing fixes?

--- js/components/admin.mjs
+++ js/components/admin.mjs
@@ -1,51 +1,33 @@
-function getPrefix() {
-    const url = window.location.href;
-    if (url.includes('github.io')) {
-        return '/FED1-PE1-Andreasjoa23';
-    }
-    return '';
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-    const prefix = getPrefix();
-
-    const loginNavLink = document.querySelector(`nav ul li a[href="${prefix}/account/login.html"]`);
-    const registerNavLink = document.querySelector(`nav ul li a[href="${prefix}/account/register.html"]`);
+    const loginNavLink = document.getElementById('loginLink');
+    const registerNavLink = document.getElementById('registerLink');
 
     function updateNavBar() {
         const userInfo = JSON.parse(localStorage.getItem('userInfo'));
 
-        if (loginNavLink && registerNavLink) {
-            if (userInfo && userInfo.accessToken) {
-                loginNavLink.textContent = 'Logout';
+        if (userInfo && userInfo.accessToken) {
+            loginNavLink.textContent = 'Logout';
+            registerNavLink.textContent = 'Create Post';
+            registerNavLink.href = '../post/create.html';
 
-                registerNavLink.textContent = 'Create Post';
-                registerNavLink.href = `${prefix}/post/create.html`;
+            loginNavLink.addEventListener('click', function(event) {
+                event.preventDefault();
+                logout();
+            });
 
-                loginNavLink.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    logout();
-                });
-
-            } else {
-                loginNavLink.textContent = 'Login';
-                loginNavLink.href = `${prefix}/account/login.html`;
-                registerNavLink.textContent = 'Register';
-                registerNavLink.href = `${prefix}/account/register.html`;
-            }
         } else {
-            console.error('One or both navigation links were not found in the DOM.');
+            loginNavLink.textContent = 'Login';
+            loginNavLink.href = '/account/login.html';
+            registerNavLink.textContent = 'Register';
+            registerNavLink.href = '/account/register.html';
         }
     }
 
     const logout = () => { 
         localStorage.removeItem('userInfo');
-        if (window.location.pathname === `${prefix}/index.html`) { 
-            window.location.href = `${prefix}/index.html`;
-        } else {
-            window.location.href = `${prefix}/post/index.html`;
+        (window.location.pathname !== '/index.html'); {
+            window.location.href = '/index.html';
         }
     };
-
     updateNavBar();
 });
