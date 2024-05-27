87e6bda1b3a21d90753407b76e64bfe9a48d24cd
andjoa00220@stud.noroff.no <andjoa00220@stud.noroff.no>
2024-05-27 12:06:01 +0200

    pathing fixes

--- js/components/admin.mjs
+++ js/components/admin.mjs
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const loginNavLink = document.querySelector('nav ul li a[href="/account/login.html"]');
-    const registerNavLink = document.querySelector('nav ul li a[href="/account/register.html"]');
+    const loginNavLink = document.querySelector('nav ul li a[href="./account/login.html"]');
+    const registerNavLink = document.querySelector('nav ul li a[href="./account/register.html"]');
 
     function updateNavBar() {
         const userInfo = JSON.parse(localStorage.getItem('userInfo'));
@@ -8,7 +8,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (userInfo && userInfo.accessToken) {
             loginNavLink.textContent = 'Logout';
             registerNavLink.textContent = 'Create Post';
-            registerNavLink.href = '/post/create.html';
+            registerNavLink.href = './post/create.html';
 
             loginNavLink.addEventListener('click', function(event) {
                 event.preventDefault();
@@ -17,18 +17,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
         } else {
             loginNavLink.textContent = 'Login';
-            loginNavLink.href = '/account/login.html';
+            loginNavLink.href = './account/login.html';
             registerNavLink.textContent = 'Register';
-            registerNavLink.href = '/account/register.html';
+            registerNavLink.href = './account/register.html';
         }
     }
 
     const logout = () => { 
         localStorage.removeItem('userInfo');
-        if (window.location.pathname === '/index.html') { 
-            window.location.href = '/index.html';
+        if (window.location.pathname === './index.html') { 
+            window.location.href = './index.html';
         } else {
-            window.location.href = '/post/index.html';
+            window.location.href = './post/index.html';
         }
     };
     updateNavBar();
